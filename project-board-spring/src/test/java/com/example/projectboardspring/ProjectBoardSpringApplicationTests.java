84abcc12cd5237f180ea0617f9370c61ea1a9957
eddie <eddie@ondelab.com>
2023-03-23 21:12:59 +0900

    스프링 부트 빌드 시작

---
+++ project-board-spring/src/test/java/com/example/projectboardspring/ProjectBoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectboardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBoardSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
