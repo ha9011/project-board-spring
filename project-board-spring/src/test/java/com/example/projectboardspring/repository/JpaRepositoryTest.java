ee0e2ce74af71d664e7d7d0e71844c181d517852
eddie <eddie@ondelab.com>
2023-03-27 23:11:40 +0900

    테스트 코드

--- project-board-spring/src/test/java/com/example/projectboardspring/repository/JpaRepositoryTest.java
+++ project-board-spring/src/test/java/com/example/projectboardspring/repository/JpaRepositoryTest.java
@@ -28,18 +28,60 @@ class JpaRepositoryTest {
         this.articleCommentRepository = articleCommentRepository;
     }
 
-    @DisplayName("select test")
+    @DisplayName("insert test")
     @Test
     void givenTestData_whenSelecting_thenWorksFine(){
         // Given
+        long prevCount = articleRepository.count();
 
         // When
+        Article saveArticle = articleRepository.save(Article.of("new article", "new content", "new hashtag"));
         List<Article> articles = articleRepository.findAll();
 
         // Then
-        assertThat(articles)
-                .isNotNull()
-                .hasSize(123);
+        assertThat(articleRepository.count())
+                .isEqualTo(prevCount + 1);
     }
 
+    @DisplayName("update test")
+    @Test
+    void givenTestData_whenUpdating_thenWorksFine(){
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        String updateHashTag = "#SpringBoot";
+        article.setHashtag(updateHashTag);
+
+        // When
+        Article saveArticle = articleRepository.save(article);
+        //Article saveArticle = articleRepository.saveAndFlush(article);
+
+        // Then
+        assertThat(saveArticle.getHashtag())
+                .isEqualTo(updateHashTag);
+
+        assertThat(saveArticle)
+                .hasFieldOrPropertyWithValue("hashtag", updateHashTag);
+    }
+
+
+    @DisplayName("delete test")
+    @Test
+    void givenTestData_whenDeleting_thenWorksFine(){
+        // Given
+        Article article = articleRepository.findById(1L).orElseThrow();
+        long prevArticleCount = articleRepository.count();
+        long prevArticleCommentCount = articleCommentRepository.count();
+        int deletedCommentsSize = article.getArticleComments().size();
+
+
+        // When
+        articleRepository.delete(article);
+
+        // Then
+        assertThat(articleRepository.count())
+                .isEqualTo(prevArticleCount-1);
+
+        assertThat(articleCommentRepository.count())
+                .isEqualTo(prevArticleCommentCount-deletedCommentsSize);
+    }
 }
